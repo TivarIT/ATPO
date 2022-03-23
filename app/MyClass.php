a7e72a73ab7d8eb9d2db263b661a61402b81207f
Tsimafei Novik <timoxa.novik03@gmail.com>
2022-03-23 13:19:20 +0300

    uodate

--- app/MyClass.php
+++ app/MyClass.php
@@ -1,8 +1,11 @@
 <?php 
 namespace App;
 class MyClass{
-    public function multiply($x){
-        return $x * 2;
+    public function multiply($x, $y){
+        return $x * $y;
+    }
+    public function division($x, $y){
+        return $x / $y;
     }
 }
 ?>
--- tests/MyClassTest.php
+++ tests/MyClassTest.php
@@ -2,8 +2,13 @@
 class MyClassTest extends PHPUnit\Framework\TestCase{
     public function testMultiply(){
         $myclass = new App\MyClass;
-        $result = $myclass->multiply(6);
-        $this->assertEquals(12, $result);
+        $result1 = $myclass->multiply(6, 2);
+        $this->assertEquals(12, $result1);
+    }
+    public function testDivision(){
+        $myclass = new App\MyClass;
+        $result2 = $myclass->division(6, 2);
+        $this->assertEquals(3, $result2);
     }
 }
 ?>
